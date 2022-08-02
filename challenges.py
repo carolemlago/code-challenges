ac1ab02733359d5e54803a2d598bc1fc9c5a0924
Carole Mendes Lago <carolemlago@gmail.com>
2022-08-01 18:12:18 -0700

    More challenges

--- challenges.py
+++ challenges.py
@@ -93,3 +93,14 @@ def maxProfit(self, prices: List[int]) -> int:
             right += 1
             
         return max_profit
+
+
+# Codewars
+# The two oldest ages function/method needs to be completed. It should take an array of numbers as its argument and return the two highest numbers within the array. The returned value should be an array in the format [second oldest age,  oldest age].
+# The order of the numbers passed in could be any order. The array will always include at least 2 items. If there are two or more oldest age, then return both of them in array format.
+
+def two_oldest_ages(ages):
+    ages.sort()
+    
+    
+    return ages[-2::]
