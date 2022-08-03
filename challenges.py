14b6c5870ca72580a9b6319c1fd05685f433e29f
Carole Mendes Lago <carolemlago@gmail.com>
2022-08-02 18:21:20 -0700

    Day 1 Code Challenge

--- challenges.py
+++ challenges.py
@@ -131,3 +131,17 @@ def two_oldest_ages(ages):
     ages.sort()
     
     return ages[-2::]
+
+
+# Convert string to camel case
+"""Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case)."""
+
+def to_camel_case(text):
+    #your code here
+
+    words = text.replace("-", " ").replace("_", " ") 
+    str = words.split(" ") 
+    if len(text) == 0:
+        return text
+    return str[0] + ''.join(i.capitalize() for i in str[1:])
+    
