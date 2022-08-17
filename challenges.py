31e7d0120ae0ae40195e7e3bf29695f991ddbb1d
Carole Mendes Lago <carolemlago@gmail.com>
2022-08-17 14:51:43 -0700

    Day 15 of Code Challenge

--- challenges.py
+++ challenges.py
@@ -565,4 +565,20 @@ def digital_root(n):
         return digital_root(res)
     else:
         return res
-        
+
+# First non-repeating character
+""" Write a function named first_non_repeating_letter that takes a string input, and returns the first character that is not repeated anywhere in the string.
+
+For example, if given the input 'stress', the function should return 't', since the letter t only occurs once in the string, and occurs first in the string.
+
+As an added challenge, upper- and lowercase letters are considered the same character, but the function should return the correct case for the initial letter. For example, the input 'sTreSS' should return 'T'.
+
+If a string contains all repeating characters, it should return an empty string ("") or None -- see sample tests. """
+
+def first_non_repeating_letter(string):
+    str_lst = [i.lower() for i in string]
+    for i in range(len(str_lst)):
+        if str_lst.count(str_lst[i]) == 1:
+            return string[i]
+    return ""
+                
