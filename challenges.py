61fc9a21a236e22f18058103f116ad87e7516f65
Carole Mendes Lago <carolemlago@gmail.com>
2022-08-18 18:30:01 -0700

    Day 16 of Code Challenge

--- challenges.py
+++ challenges.py
@@ -308,6 +308,29 @@ class Solution:
             dp[i] = 1 + dp[i-offset]
         return dp
 
+# 141. Linked List Cycle       
+""" Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false. """ 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # use fast and slow pointers, and if they meet at some point, then it's a cycle
+        slow = fast = head
+        while slow.next and fast.next and fast.next.next:
+            fast = fast.next.next
+            slow = fast
+            if slow == fast:
+                return True
+        return False
+
 # Hacker Rank
 
 # There is a large pile of socks that must be paired by color. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
