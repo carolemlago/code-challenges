941ef8cff3361d5773c317ee906f8a53763f86d1
Carole Mendes Lago <carolemlago@gmail.com>
2022-08-25 20:03:15 -0700

    Day 22 of Code Challenge

--- challenges.py
+++ challenges.py
@@ -449,7 +449,26 @@ class Solution:
             else:
                 cur.next = cur.next.next
         return head
-                
+
+# L142. Linked List Cycle II
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        node_list = set()
+
+        while head:
+            if head in node_list:
+                return head
+            
+            node_list.add(head)
+            head = head.next
+        
+        return None
 
 # Hacker Rank
 
