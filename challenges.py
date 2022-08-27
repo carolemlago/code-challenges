89072bd5d8145e2f89e332a134752abf64b2ec79
Carole Mendes Lago <carolemlago@gmail.com>
2022-08-27 14:23:21 -0700

    Day 24 of Code Challenge

--- challenges.py
+++ challenges.py
@@ -1,5 +1,19 @@
 # Leet Code
 
+#  Valid Palindrome
+
+"""A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise."""
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        newString = ""
+        
+        for c in s:
+            if c.isalnum():
+                newString += c.lower()
+        return newString == newString[::-1]
+
 
 # Valid Anagram
 
